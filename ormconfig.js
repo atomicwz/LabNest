8edfe990415075d4e6f7f140371b728b43a281cd
Dener <dener@startapp.one>
2023-06-08 10:56:09 -0300

    Att: url deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: 'dpg-ci0ti4g2qv21rs7kqh3g-a',
+  host: 'postgres://fish:taRzczT9RSZW4DxUfZurRlLjsxW3intY@dpg-ci0ti4g2qv21rs7kqh3g-a.oregon-postgres.render.com/dash_z5xn',
   port: 5432,
   username: 'taRzczT9RSZW4DxUfZurRlLjsxW3intY',
   password: 'fish',
