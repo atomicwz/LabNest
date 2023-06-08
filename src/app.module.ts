220a15c76cf9da78850cd50897de0c9141ba544c
Dener <dener@startapp.one>
2023-06-08 19:51:19 -0300

    Fix: host address

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'postgres',
       port: 5432,
-      host: process.env.HOST || 'dpg-ci0ti4g2qv21rs7kqh3g-ay',
+      host: process.env.HOST || 'dpg-ci0ti4g2qv21rs7kqh3g-a',
       username: process.env.USERDB || 'fish',
       password: process.env.PASSWORD || 'taRzczT9RSZW4DxUfZurRlLjsxW3intY',
       database: process.env.DATABASE || 'dash_z5xn',
