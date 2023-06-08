dfb602117ee475dcfb5056faa6484b2e68a013c1
Dener <dener@startapp.one>
2023-06-08 11:06:43 -0300

    try-deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'postgres',
-  host: 'postgres://fish:taRzczT9RSZW4DxUfZurRlLjsxW3intY@dpg-ci0ti4g2qv21rs7kqh3g-a/dash_z5xn',
+  host: 'dpg-ci0ti4g2qv21rs7kqh3g-a',
   port: 5432,
   username: 'taRzczT9RSZW4DxUfZurRlLjsxW3intY',
   password: 'fish',
