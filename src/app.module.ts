08849474c46883619c37a4d6e0aaf509fdec05d3
Dener <dener@startapp.one>
2023-06-08 15:07:31 -0300

    Att: fix build and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 @Module({
